950727b60701b5fff6bf380d0f00038fc74bfffe
Richard Albritton <richardalbritton@gmail.com>
2018-07-06 09:22:05 -0700

    Add files via upload

---
+++ nodemcu_text_scroll/nodemcu_text_scroll.ino
@@ -0,0 +1,120 @@
+// Adafruit IO Publish & Subscribe Example
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN D7
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+  
+/************************ Example Starts Here *******************************/
+
+// set up the 'counter' feed
+AdafruitIO_Feed *text = io.feed("text");
+
+void setup() {
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(40);
+  matrix.setTextColor(colors[0]);
+
+  // wait for serial monitor to open
+ // while(! Serial);
+
+  Serial.print("Connecting to Adafruit IO");
+
+  // start MQTT connection to io.adafruit.com
+  io.connect();
+
+  // set up a message handler for the count feed.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+  text->onMessage(handleMessage);
+
+  // wait for an MQTT connection
+  // NOTE: when blending the HTTP and MQTT API, always use the mqttStatus
+  // method to check on MQTT connection status specifically
+  while(io.mqttStatus() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // Because Adafruit IO doesn't support the MQTT retain flag, we can use the
+  // get() function to ask IO to resend the last value for this feed to just
+  // this MQTT client after the io client is connected.
+  text->get();
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+int x    = matrix.width();
+int pass = 0;
+char* MSGbuffer;
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+  // Because this sketch isn't publishing, we don't need
+  // a delay() in the main program loop.
+  int len = strlen(MSGbuffer);
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 0);
+  //matrix.print(F("blaaaaa!"));
+  matrix.print(MSGbuffer);
+  if(--x < (len * -6)) {
+    x = matrix.width();
+    if(++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+  matrix.show();
+  delay(0);
+
+}
+
+// this function is called whenever a 'counter' message
+// is received from Adafruit IO. it was attached to
+// the counter feed in the setup() function above.
+void handleMessage(AdafruitIO_Data *data) {
+
+  MSGbuffer = data->value();
+  Serial.print("received <- ");
+  Serial.println(MSGbuffer);
+  
+}
